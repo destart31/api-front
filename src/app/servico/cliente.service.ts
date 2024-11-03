4c28a772fbccb8c84419af07b92eefc10631847a
Arthur Gabriel <arthurgabriel31@hotmail.com>
2024-11-03 16:19:14 -0300

    Listagem de Clientes Parte 1

--- src/app/servico/cliente.service.ts
+++ src/app/servico/cliente.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Cliente } from '../modelo/Cliente';
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +11,10 @@ export class ClienteService {
   // Url da API
   private url:string = 'http://localhost:8080';
 
+  // Construtor
   constructor(private http:HttpClient) { }
+  // Metodo para selecionar todos os clientes
+  selecionar():Observable<Cliente[]>{
+    return this.http.get<Cliente[]>(this.url);
+  }
 }
