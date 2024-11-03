d2a7226cf79b7aaaf4e46befd019d3667ab7bbb5
Arthur Gabriel <arthurgabriel31@hotmail.com>
2024-11-03 19:46:26 -0300

    Edição de clientes Parte 1

--- src/app/servico/cliente.service.ts
+++ src/app/servico/cliente.service.ts
@@ -22,4 +22,8 @@ export class ClienteService {
   cadastrar(obj:Cliente):Observable<Cliente>{
     return this.http.post<Cliente>(this.url, obj);
 }
+  // Metodo para editar clientes
+  esditar(obj:Cliente):Observable<Cliente>{
+    return this.http.put<Cliente>(this.url, obj);
+  }
 }
