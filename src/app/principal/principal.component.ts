eb920b7efc75e1afa8c3b2ca1c41937eff99daba
Arthur Gabriel <arthurgabriel31@hotmail.com>
2024-11-03 16:25:33 -0300

    Listagem de Clientes Parte 2

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Cliente } from '../modelo/Cliente';
+import { ClienteService } from '../servico/cliente.service';
 
 @Component({
   selector: 'app-principal',
@@ -15,4 +17,15 @@ import { FormsModule } from '@angular/forms';
 export class PrincipalComponent {
   // Variavel para visibilidade dos botões
   btnCadastro:boolean = true;
+
+  // Json de clientes
+  clientes:Cliente[] = [];
+
+  // Construtor
+  constructor(private servico:ClienteService) {}
+
+  // Metodo de selecionar os clientes
+  selecionar():void {
+    this.servico.selecionar().subscribe(retorno => this.clientes = retorno);
+  }
 }
